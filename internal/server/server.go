287554f2d83bb07772c2dfe2e9e599e98cc1be54
Naviz Khoja <naviz.khoja@eventbeep.com>
2023-10-03 00:19:19 +0530

    added layout templates and couple of functions

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,34 +2,47 @@ package server
 
 import (
 	"flag"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/template/html/v2"
+	"github.com/gofiber/websocket/v2"
+	"github.com/naveeez10/vidchat/internal/handlers"
 	"os"
 	"time"
 )
 
 var (
-	addr = flag.String("addr,":"", os.Getenv("PORT"), "")
-	cert = flag.String("cert" , "", "")
-	key =  flag.String("key" , "", "")
+	addr = flag.String("addr", ":", os.Getenv("PORT"))
+	cert = flag.String("cert", "", "")
+	key  = flag.String("key", "", "")
 )
-
 
 func Run() error {
 	flag.Parse()
 
 	if *addr == ":" {
-		addr = ":8080"
+		*addr = ":8080"
 	}
 
-	app.Get("/",handlers.Welcome)
+	engine := html.New("./views", ".html")
+	app := fiber.New(fiber.Config{Views: engine})
+	app.Use(logger.New())
+	app.Use(cors.New())
+
+	app.Get("/", handlers.Welcome)
 	app.Get("/room/create", handlers.RoomCreate)
 	app.Get("/room/:uuid", handlers.Room)
-	app.Get("/room/:uuid/websocket", )
+	app.Get("/room/:uuid/websocket", websocket.New(handlers.
+		RoomWebsocket, websocket.Config{
+		HandshakeTimeout: 10 * time.Second,
+	}))
 	app.Get("/room/:uuid/chat", handlers.RoomChat)
 	app.Get("/room/:uuid/chat/websocket", websocket.New(handlers.RoomChatWebSocket))
 	app.Get("/room/uuid/viewer/websocket", websocket.New(handlers.RoomViewWebSocket))
 	app.Get("/stream/:ssuid", handlers.Stream)
 	app.Get("/stream/:ssuid/websocket")
-	app.Get("/stream/ssuid/chat/websocket",)
-	app.Get("/stream/:ssuid/viewer/websocket",)
+	app.Get("/stream/ssuid/chat/websocket")
+	app.Get("/stream/:ssuid/viewer/websocket")
 
 }
